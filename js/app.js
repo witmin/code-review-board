e7ac7b0cb0b5aca3698ac308033f0342a733d4ea
Millie Lin <witmin@gmail.com>
2014-07-13 22:06:23 +0800

    Updated angular.min.js, Updated the angularjs structure.

--- js/app.js
+++ js/app.js
@@ -1,88 +1,106 @@
 /**
  * Created by millie.lin on 11/12/13.
  */
-var app = angular.module("codeReviewBoard", ["firebase"]);
 
-function MainController($scope, angularFire ){
-    var ref = new Firebase('https://code-review-firebase.firebaseio.com/');
-//  Set up review item
-    $scope.item = [];
-    angularFire(ref, $scope, "item");
+(function(){
+    var app = angular.module("codeReviewBoard", ["firebase"]);
 
-//  Get Date and Time through moment.js
-    $scope.date = moment().calendar();
+    var ref = new Firebase('https://code-review-firebase.firebaseio.com/reviews');
 
-//  Coder is the user who post the review request
-    $scope.coderName = '';
-    $scope.reviewerName = '';
+    
+    app.controller("ReviewController", function($firebase){
+        this.reviews = [];
+        this.reviews = $firebase(ref);
+        this.removeReview = function(){
+            ref.remove();
+        };
 
-//  Add
-    $scope.addReviewItem = function(e){
-        if(e.keyCode != 13) return;
-        $scope.item.push({
-            user: $scope.coderName,
-            description: $scope.description,
-            status: 'waiting',
-            createdTime : $scope.date,
-            reviewer: '',
-            startReviewTime: '',
-            endReviewTime:''
-            });
-        pushAddNotify(ref);
-        $scope.description = '';
-    };
-    $scope.addReviewItemClick = function(){
-        $scope.item.push({
-            user: $scope.coderName,
-            description: $scope.description,
-            status: 'waiting',
-            reviewer: '',
-            startReviewTime: '',
-            endReviewTime:''
-        });
-        pushAddNotify(ref);
-        $scope.description = '';
-    };
-
-//  Remove
-    $scope.removeItem = function() {
-        $scope.item.splice($scope.toRemove, 1);
-        $scope.toRemove = null;
-    };
-
-//  Click "Start Review" to add reviewer's name and to change status
-    $scope.startReviewClick = function(index){
-        ref.index.update({
-            reviewer: $scope.reviewerName,
-            status: 'under review'
-        });
-    };
-}
-//    Show Webkit Notification When new database comes in
-function pushAddNotify(db){
-    db.on('child_added', function(snapshot){
-        var userName = snapshot.val().user;
-        var content = snapshot.val().description;
-        addNotify(userName, content);
+    //    this.startReviewClick = startReviewClick;
     });
-}
-//Webkit Notification
-function addNotify(user, description) {
-    var havePermission = window.webkitNotifications.checkPermission();
-    if (havePermission == 0) {
-        // 0 is PERMISSION_ALLOWED
-        var notification = window.webkitNotifications.createNotification(
-            'images/notification-new.png',
-            user + ' needs code review',
-            description
-        );
 
-        notification.onclick = function () {
-            window.open("index.html");
-            notification.close();
-        }
-        notification.show();
-    } else {
-        window.webkitNotifications.requestPermission();
+    app.controller("RequestController", function(){
+        // Get Date and Time through moment.js
+        var date = moment().calendar();
+        this.review = {};
+        this.review = {
+            'status': 'waiting',
+            'createdTime': date,
+            'reviewer': '(To be confirmed)',
+            'startReviewTime': ' ',
+            'endReviewTime': ' '
+        };
+        this.addRequest= function(){
+            ref.push(this.review);
+            this.review = {};
+        };
+    });
+
+    app.controller("ReviewerController", function () {
+        var date = moment().calendar();
+        this.reviewer = {};
+        this.addReviewer= function(){
+            ref.child().set({
+                "reviewerName": this.reviewer.reviewerName,
+                "startReviewTime": date
+            });
+            this.reviewer = {};
+        };
+    });
+    
+    // Show/hide Reviewer Dialog
+    var reviewerDialog = {
+        'show': false
     }
-}
+
+    //  Add
+    var addReviewItem = function(e){
+        if(e.keyCode != 13) return;
+        reviews.push({
+            user: $scope.coderName,
+            description: $scope.description,
+            status: 'waiting',
+            createdTime : new date(),
+            reviewer: '',
+            startReviewTime: '',
+            endReviewTime:''
+        });
+        pushAddNotify(reviews);
+//        description = '';
+    };
+
+    //  Remove
+    var removeItem = function() {
+        reviews.splice(this.toRemove, 1);
+        this.toRemove = null;
+    };
+
+    //    Show Webkit Notification When new database comes in
+    function pushAddNotify(db){
+        db.on('child_added', function(snapshot){
+            var userName = snapshot.val().user;
+            var content = snapshot.val().description;
+            addNotify(userName, content);
+        });
+    }
+
+    //Webkit Notification
+    function addNotify(user, description) {
+        var havePermission = window.webkitNotifications.checkPermission();
+        if (havePermission == 0) {
+            // 0 is PERMISSION_ALLOWED
+            var notification = window.webkitNotifications.createNotification(
+                'images/notification-new.png',
+                user + ' needs code review',
+                description
+            );
+
+            notification.onclick = function () {
+                window.open("index.html");
+                notification.close();
+            }
+            notification.show();
+        } else {
+            window.webkitNotifications.requestPermission();
+        }
+    }
+})()
